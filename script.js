533eba62607676b145079d293a9c85ed951f1ec2
Zak <125280277+Zairel38@users.noreply.github.com>
2026-05-01 16:03:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function learnMore() {
     alert("Welcome to Avant-Lenz!");
 }
+function scrollToFeatures() {
+    document.getElementById("features").scrollIntoView({
+        behavior: "smooth"
+    });
+}
