8e84aa7076ff0ee033972979fd977d2184c86109
Zak <125280277+Zairel38@users.noreply.github.com>
2026-05-02 13:55:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("JS loaded successfully");
+function goToProducts() {
+    window.location.href = "products.html";
+}
+
+function sendMessage() {
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+    const output = document.getElementById("contactMsg");
+
+    if (!name || !message) {
+        output.innerText = "Please fill all fields.";
+    } else {
+        output.innerText = "Message sent successfully!";
+    }
+}
