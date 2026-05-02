ad8b26debf2a40ffcba87a329200de3c2dfa9089
Zak <125280277+Zairel38@users.noreply.github.com>
2026-05-01 21:13:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,3 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll(".fade-in").forEach(el => {
     observer.observe(el);
 });
-.fade-in {
-    opacity: 0;
-    transform: translateY(30px);
-    transition: all 0.8s ease;
-}
-
-.fade-in.show {
-    opacity: 1;
-    transform: translateY(0);
-}
